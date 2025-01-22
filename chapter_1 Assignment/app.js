709ac29daa89e7e7d16560424bdecc95e6bf5047
Syed Abdullah <syed.abdullah7638@gmail.com>
2025-01-22 21:20:31 +0500

    Make Alerts, Document.write, console

---
+++ chapter_1 Assignment/app.js
@@ -0,0 +1,42 @@
+
+// console.log("Assigmnet 1");
+
+
+
+// 1. Write a script to greet your website visitor using JS alert box.
+
+// alert("Hello! Thanks to visit our website.");
+
+
+// 2. Write a script to display following message on your web page
+
+// alert("Error! Please enter a valid password.")
+
+
+
+// 3. Write a script to display following message on your web page
+
+// document.write("Welcome to Js land..." + "<br />" + "Happy coding!")
+
+
+// 4. Write a script to display following messages in sequence:
+
+// prompt("Welcome to Js land..");
+// prompt("Happy coding!");
+
+
+// 5. Generate the following message through browser’s developer console
+
+// console.log("Hello.. Ican run Js through developers console.");
+
+
+// 6. Make use of alerts in your new/existing HTML & CSS project.
+
+// alert("Thanks for visiting our website!");
+
+
+// 7. Practice placement of <script></script> element in following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML) 
+// d. Body (after your page’s HTML)
