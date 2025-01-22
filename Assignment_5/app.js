184b9888d9fecd544242a17a57aafa515620324c
Syed Abdullah <syed.abdullah7638@gmail.com>
2025-01-23 00:08:11 +0500

    Assignment 5 half

---
+++ Assignment_5/app.js
@@ -0,0 +1,134 @@
+console.log("Hello assignment 5");
+
+
+
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+
+// var num1 = 10;
+// var num2 = 20;
+
+// var sum = num1 + num2;
+
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+
+
+
+
+
+
+// 2. Repeat task1 for subtraction, multiplication, division & modulus.
+
+// // subtraction 
+// var num1 = 10;
+// var num2 = 20;
+
+// var sub = num1 - num2;
+
+// document.write("difference of " + num1 + " and " + num2 + " is " + sub);
+
+
+// Multiplication
+// var num1 = 10;
+// var num2 = 20;
+
+// var mul = num1 * num2;
+
+// document.write("multiplication of " + num1 + " and " + num2 + " is " + mul);
+
+
+// division
+// var num1 = 10;
+// var num2 = 20;
+
+// var div = num1 / num2;
+
+// document.write("division of " + num1 + " and " + num2 + " is " + div);
+
+
+
+// Moud
+// var num1 = 10;
+// var num2 = 20;
+
+// var moud = num1 % num2;
+
+// document.write("moud of " + num1 + " and " + num2 + " is " + moud);
+
+
+
+
+
+
+
+
+
+
+// 3. Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value by 3. 
+// l. Output : “The remainder is : 0”.
+
+
+
+
+// var num;
+// document.write("Value after variable declarartion is " + num + "<br />");
+
+// num = 5;
+// document.write("Initial value: " + num + "<br />");
+
+// num++;
+// document.write("Value after increment is: " + num + "<br />");
+
+// num = num + 7;
+// document.write("Value after +7 is: " + num + "<br />");
+
+
+// num--;
+// document.write("Value after decreament is: " + num + "<br />");
+
+
+// num = num % 3;
+// document.write("The remainder is : " + num + "<br />");
+
+
+
+
+
+
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to store
+// ticket price in a variable & calculate the cost of buying 5 tickets
+
+// var price = 600;
+// var quantity = 5;
+// var total = price * quantity + "PKR";
+
+// document.write ("Total cost of " + quantity + " tickets is " + total)
+
+
+
+
+
+
+
+
+// 5. Write a script to display multiplication table of any number in your browser.
+
+// var table = 2;
+// var startingPoint = 4;
+// var endingPoint = 10;
+
+// for(var i = startingPoint; i <= endingPoint; i++) {
+//     document.write(table + " X " + i + " = " + i*table + "<br />")
+// }
