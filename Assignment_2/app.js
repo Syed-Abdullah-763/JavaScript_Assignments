54c152cdb9cb0325ccb2448c8053c0f580644794
Syed Abdullah <syed.abdullah7638@gmail.com>
2025-01-22 22:33:23 +0500

    Variables

---
+++ Assignment_2/app.js
@@ -0,0 +1,89 @@
+// console.log("Hello Assignment 2")
+
+
+// 1. Declare a variable called username.
+// var userName = "Abdullah";
+
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName = "Syed Abdullah";
+
+
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// var message = "Hello world";
+// alert(message);
+
+
+
+
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+// var studentName = "Abdullah";
+// var age = 18;
+// var certification = "Certified MERN Stack Development";
+
+// alert(studentName);
+// alert(age);
+// alert(certification);
+
+
+
+
+
+// 5. Write a script to display the following alert using one JS variable
+
+// var pizza = "pizza";
+// for (var i = 0; i < pizza.length; i++) {
+//     console.log(pizza.slice(0, i + 1));
+// }
+
+// for (var i = pizza.length; i >= 0; i--) {
+//     console.log(pizza.slice(0, i));
+// }
+
+
+
+
+
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).
+
+// var message = "My Email Adress is";
+// var email = "syed.abdullah7638@gmail.com";
+
+// alert(message + " " + email);
+
+
+
+
+
+
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+
+
+
+
+
+// 8. Write a script to display this in browser through JS
+
+// document.write("Yah! I can write HTML content through Js.")
+
+
+
+
+// 9. Store following string in a variable and show in alert and browser through JS
+
+// var str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(str);
