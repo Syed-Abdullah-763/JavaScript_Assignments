be654f01e5347c83aa4dab96cca802ed2f541a15
Syed Abdullah <syed.abdullah7638@gmail.com>
2025-01-22 23:18:55 +0500

    Legal and illegal variables name

---
+++ Assignment_4/app.js
@@ -0,0 +1,46 @@
+// console.log("Hello assignment 4");
+
+
+
+
+// 1. Declare 3 variables in one statement.
+// var x = 10, y = 20, z = 30;
+
+
+
+
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// legal
+
+// var age = 10;
+// var myAge = 17;
+// var my_age = 17;
+// var _name = "Abdullah";
+
+
+// illegal 
+
+// var 1name = "koch bhi";
+// var my name = "koch bhi";
+// var for = "koch bhi";
+// var while = "koch bhi";
+// var if = "koch bhi";
+
+
+
+
+// 3. Display this in your browser
+// a)  A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______, ______ and ______.
+// c) Variables must begin with a ______, ______ or _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+// document.write("Rules For Naming JS Variables:" + "<br />")
+// document.write(" 1. Variable names can only contain characters, numbers and underscore." + "<br />");
+// document.write(" 2. Variables must begin with a character, number or undercore. For example $name, _name or name" + "<br />");
+// document.write(" 3. Variable names are case sensitive" + "<br />");
+// document.write(" 4. Variable names should not be JS reserved words" + "<br />");
