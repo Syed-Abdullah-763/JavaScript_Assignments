378a882cecbb415a0638bb39189263cd50aa2ca9
Syed Abdullah <syed.abdullah7638@gmail.com>
2025-01-22 22:58:32 +0500

    Variables with number

---
+++ Assignment_3/app.js
@@ -0,0 +1,51 @@
+// console.log("Hello assignment 3");
+
+
+
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+// var age = 17;
+// alert(age);
+
+
+
+
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+
+// var visiteNumbers;
+// visiteNumbers = 14;
+
+// alert("You have visited " + visiteNumbers + " times")
+
+
+
+
+
+
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+// var birthYear = 2007;
+
+// document.write("My birth year is " + birthYear + "<br />" + "the declared variables datatype is number.")
+
+
+
+
+
+
+
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”
+
+
+// var visitorName = "Abdullah";
+// var product = "Laptop(s)";
+// var quantity = 10;
+
+// document.write( visitorName + " ordered " + quantity + " " + product + " on XYZ Clothing store")
